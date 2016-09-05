2ac9ee4d06a4b2abf3b46f1693b3d9be3a7e5d46
Colin Brown <colinbrown@dhcp-v017-009.mobile.uci.edu>
2016-09-05 11:33:40 -0700

    Converted portfolio entries to templates
    - Added mustache.js
    - Added template.html
    - Externalized portfolio item’s data to json

---
+++ portfolio/js/template-othello.js
@@ -0,0 +1,8 @@
+function applyTemplate() {
+    $.getJSON('json/othello.json', function(data) {
+        $.get('template.html', function(template) {
+            var html = Mustache.to_html(template, data);
+            $('#contents').html(html);
+        });
+    });
+};
